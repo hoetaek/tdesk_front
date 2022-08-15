ced07ff5a2492c4d1eff989c34e96eadadf78711
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-15 14:57:26 +0900

    margin change

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import NavbarHeader from "@/components/NavbarHeader.vue";
 </script>
 
 <template>
-  <div class="max-w-7xl mx-auto px-2">
+  <div class="mx-56 px-2">
     <navbar-header></navbar-header>
     <router-view />
   </div>
