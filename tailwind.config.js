923119db131891aa9077f003fad872f4149f79ef
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-15 16:05:36 +0900

    feat: color customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     colors: {
       "light-grey": "#6B6B6B",
+      primary: "#2690FF",
+      white: colors.white,
     },
   },
   plugins: [],
