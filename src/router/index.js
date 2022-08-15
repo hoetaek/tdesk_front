13a5aed7a2b0d39e522c90c185707f8aab95fad6
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-15 14:53:36 +0900

    feat: router setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    },
+    {
+      path: "/worksheet",
+      name: "worksheet",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/WorksheetView.vue"),
+    },
+    {
+      path: "/wordsearch",
+      name: "wordsearch",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/WordsearchView.vue"),
+    },
+    {
+      path: "/suggestion",
+      name: "suggestion",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/SuggestionView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
