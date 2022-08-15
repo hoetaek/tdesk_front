54435a1ce1a26026c445718ee49e2e3917ba1e9b
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-15 14:53:49 +0900

    feat: basic navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main>Hello</main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "light-grey": "#6B6B6B",
+    },
   },
   plugins: [],
 };
